4bd47fabd7bd52736ac143e750b135db0b22b746
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-12-26 17:34:45 -0500

    added CC component

---
+++ src/components/cc.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+import { Spring } from 'react-spring'
+import styled from 'styled-components'
+
+export default class CC extends Component {
+  render() {
+    return (
+      <>
+        <h1>CC</h1>
+      </>
+    )
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Hover from '../components/hover'
+import CC from '../components/cc'
 import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
@@ -9,7 +10,7 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
-
+    <CC />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
