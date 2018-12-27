fdb6f8429a994b22a1e88f382687b45319268e0c
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-12-26 23:05:37 -0500

    added TitleText styledComponent to CC

--- src/components/cc.js
+++ src/components/cc.js
@@ -2,11 +2,16 @@ import React, { Component, Fragment } from 'react'
 import { Spring } from 'react-spring'
 import styled from 'styled-components'
 
+const TitleText = styled.h1`
+  font-family: 'Roboto'
+  font-size: 600;
+`
+
 export default class CC extends Component {
   render() {
     return (
       <>
-        <h1>CC</h1>
+        <TitleText>CC</TitleText>
       </>
     )
   }
