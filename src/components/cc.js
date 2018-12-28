a13200b4dad6c06cf43a9550ab02beaf5fc959e9
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-12-27 19:23:10 -0500

    added border around CC component, added flex container for CC, imported styled-components and created ccContainer in index.js

--- src/components/cc.js
+++ src/components/cc.js
@@ -2,17 +2,27 @@ import React, { Component, Fragment } from 'react'
 import { Spring } from 'react-spring'
 import styled from 'styled-components'
 
+const TitleContainer = styled.div`
+  display: flex;
+  border: 1px solid black;
+  border-radius: 30px;
+  width: 120px;
+  height: 120px;
+  align-items: center;
+  justify-content: center;
+`
 const TitleText = styled.h1`
   font-family: 'Roboto'
+  justify-content: center;
   font-size: 600;
 `
 
 export default class CC extends Component {
   render() {
     return (
-      <>
+      <TitleContainer>
         <TitleText>CC</TitleText>
-      </>
+      </TitleContainer>
     )
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
 
 import Hover from '../components/hover'
 import CC from '../components/cc'
@@ -7,11 +8,19 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 
+const ccContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
 const IndexPage = () => (
   <>
     <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
-    <CC />
-    <Link to="/page-2/">Go to page 2</Link>
+    <ccContainer>
+      <CC />
+    </ccContainer>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </>
 )
 
