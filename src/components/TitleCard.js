3d124b3b7067b2dd6fd01237b177db6e30aafea0
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2019-01-04 13:13:37 +0100

    added TitleCard component for hover animation and CC component

---
+++ src/components/TitleCard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+export default class TitleCard extends Component {
+  render() {
+    return (
+      <div>
+        <p>I'm The TitleCard!</p>
+      </div>
+    )
+  }
+}
