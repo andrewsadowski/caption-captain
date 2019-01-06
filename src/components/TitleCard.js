62ad0c31bb9dae0d3c331d18db24f8132a7c52b4
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2019-01-06 21:19:20 +0100

    added y-axis transform react-spring to animate entrace of TitleCard

--- src/components/TitleCard.js
+++ src/components/TitleCard.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import { Spring } from 'styled-components'
 import styled from 'styled-components'
 
 export default class TitleCard extends Component {
   render() {
     return (
       <div>
-        <p>I'm The TitleCard!</p>
+        <Spring
+          from={{ opacity: 0, bgOpacity: 0, y: '-50px' }}
+          to={{ opacity: 1, bgOpacity: 0.5, y: '0px' }}
+        >
+          {
+            <p
+              style={{
+                transform: styles.y.interpolate(y => `translate3d(0, ${y}, 0)`),
+                ...styles,
+              }}
+            >
+              I'm The TitleCard!
+            </p>
+          }
+        </Spring>
       </div>
     )
   }
